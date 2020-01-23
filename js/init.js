8e6e252403ec8146db3d0d688c1dd83acb0ed2d8
Lucas Fernando <contato@lucasfjs.com>
2020-01-22 21:58:02 -0300

    Adicionando conexão com Firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCtfaU3M8CKiWO_vmVp1t-rcWryzAq1Ih8",
+    authDomain: "linkme-8a171.firebaseapp.com",
+    databaseURL: "https://linkme-8a171.firebaseio.com",
+    projectId: "linkme-8a171",
+    storageBucket: "linkme-8a171.appspot.com",
+    messagingSenderId: "1044631773778",
+    appId: "1:1044631773778:web:4c32ae8ddf6e35adb3b58b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
